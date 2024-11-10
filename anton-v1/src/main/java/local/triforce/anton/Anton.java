455e02e976bce2d67e457e032de278459cfed9c4
Simon Lehner-Dittenberger <simon.lehner-dittenberger@cloudflight.io>
2024-11-10 02:06:19 +0100

    just bad xd

--- anton-v1/src/main/java/local/triforce/anton/Anton.java
+++ anton-v1/src/main/java/local/triforce/anton/Anton.java
@@ -18,7 +18,7 @@ import java.util.*;
 import static java.lang.Math.random;
 
 public class Anton implements Runnable {
-
+    private final int rounds = 100;
     private RateAPI rate = new SimpleRateAPI();
     private SimAPI sim = new SimpleSimExample();
     private StateAPI state = new MockStateAPI();
@@ -38,11 +38,16 @@ public class Anton implements Runnable {
         List<String> bestIdeas = null;
         long t = System.currentTimeMillis();//TODO;
 
+        Map<String, List<Long>> csv = new TreeMap<>();
+
         long id = 0;
         while (true) {
             id++;
+            String prefix = "" + id;
             World world = state.generateWorld(t);
             Map<Long, Double> rates = new TreeMap<>();
+            Map<String, List<Long>> tmpcsv = new HashMap<>();
+
 
             //apply ideas
             List<String> ideas = new ArrayList<>();
@@ -59,36 +64,27 @@ public class Anton implements Runnable {
                     slope -> slope.setFunlevel(slope.getFunlevel() + skew)
             );
 
-            try {
-                PrintWriter out = new PrintWriter(new FileOutputStream("output/run" + id + ".csv"));
-
-
 //simulate for a bit
-                int rounds = 100;
-                for (int i = 0; i < rounds; i++) {
-                    world = sim.step(world);
-                    rates.put(t + i, rate.rate(world.getPeopleAtStation()));
+            for (int i = 0; i < rounds; i++) {
+                world = sim.step(world);
+                rates.put(t + i, rate.rate(world.getPeopleAtStation()));
 
-                    if(i == 0) {
-                        out.println(world.getPeopleAtStation().keySet().stream().reduce((a,b) -> a+";"+b).orElse("no data"));
-                    }
 
-                    String csv = world.getPeopleAtStation().entrySet().stream()
-                            .sorted(Map.Entry.comparingByKey())
-                            .map(station -> station.getValue().toString())
-                            .reduce((a, b) -> a + ";" + b).orElse("[no data]");
-                    out.println(csv);
+                world.getPeopleAtStation().forEach((key, v) -> {
+                    String finalkey = prefix + "/" + key;
+                    List<Long> l = tmpcsv.getOrDefault(finalkey, new ArrayList<Long>());
+                    l.add(v);
+                    tmpcsv.put(finalkey, l);
+                });
 //                System.out.printf("sim step for (%d): %s\n", id, csv);
-                }
-                out.close();
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
             }
+
             double metaRate = rate.metaRate(rates);
             if (metaRate > best) {
                 best = metaRate;
                 bestIdeas = ideas;
-                System.out.printf("found better ideas(id=%d) with rate %f, ideas: %s\n", id,  best, bestIdeas);
+                csv.putAll(tmpcsv);
+                System.out.printf("found better ideas(id=%d) with rate %f, ideas: %s\n", id, best, bestIdeas);
                 consecutiveFails = 0;
             } else {
                 consecutiveFails++;
@@ -97,6 +93,24 @@ public class Anton implements Runnable {
                 System.out.printf("giving up, best idea with rate %f is: %s\n", best, bestIdeas);
                 break;
             }
+        }
+
+
+        try {
+            PrintWriter out = new PrintWriter(new FileOutputStream("output/resultvalues.csv"));
+
+            for (int i = 0; i < rounds; i++) {
+                if (i == 0) {
+                    out.println(csv.keySet().stream().reduce((a, b) -> a + ";" + b).orElse("no data"));
+                }
+                int finalI = i;
+                out.println(csv.entrySet().stream().map(v -> v.getValue().get(finalI)).map(Object::toString).reduce((a, b) -> a + ";" + b).orElse("no data"));
+            }
+
+            out.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 
